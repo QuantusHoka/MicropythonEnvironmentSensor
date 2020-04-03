da5558130fdaaa56c71e1136f9685d9ee90fe6c3
Mark Clarkson <mark@hollowhills.net>
2020-04-03 12:38:47 -0600

    working

--- hdc1080.py
+++ hdc1080.py
@@ -3,7 +3,7 @@
 
 from micropython import const
 from machine import I2C
-from time import sleep
+from time import sleep_ms
 
 #I2C Address
 HDC1080_ADDR = const(0x40)
@@ -20,21 +20,17 @@ MFID_REG = const(0xFE)  # manufacturer ID register
 DVID_REG = const(0xFF)  # device ID register
 
 #Configuration Register Bits
-
-HDC1080_CONFIG_RESET_BIT =                      const(0x8000)
-HDC1080_CONFIG_HEATER_ENABLE =                  const(0x2000)
-HDC1080_CONFIG_ACQUISITION_MODE =               const(0x1000)
-HDC1080_CONFIG_BATTERY_STATUS =                 const(0x0800)
-HDC1080_CONFIG_TEMPERATURE_RESOLUTION =         const(0x0400)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_HBIT =       const(0x0200)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_LBIT =       const(0x0100)
-
-HDC1080_CONFIG_TEMPERATURE_RESOLUTION_14BIT =   const(0x0000)
-HDC1080_CONFIG_TEMPERATURE_RESOLUTION_11BIT =   const(0x0400)
-
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_14BIT =      const(0x0000)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_11BIT =      const(0x0100)
-HDC1080_CONFIG_HUMIDITY_RESOLUTION_8BIT =       const(0x0200)
+RESET_BIT =         const(0x8000)
+HEATER_ENABLE =     const(0x2000)
+ACQUISITION_MODE =  const(0x1000)
+TEMP_RES_14BIT =    const(0x0000)
+TEMP_RES_11BIT =    const(0x0400)
+HUM_RES_14BIT =     const(0x0000)
+HUM_RES_11BIT =     const(0x0100)
+HUM_RES_8BIT =      const(0x0200)
+# Delays\
+STARTUP_DELAY =     const(15) #ms
+MEAS_DELAY =        const(15) #ms
 
 
 class HDC1080:
@@ -44,14 +40,20 @@ class HDC1080:
         i2c -- The i2c object (driver) used to interact through device addresses.
         slave_addr -- The slave address of the sensor (default 64 or 0x40).
         """
+        self.TEMP_14BIT = TEMP_RES_14BIT
+        self.TEMP_11BIT = TEMP_RES_11BIT
+        self.HUM_14BIT = HUM_RES_14BIT
+        self.HUM_11BIT = HUM_RES_11BIT
+        self.HUM_8BIT = HUM_RES_8BIT
+
         self.i2c = i2c
         assert slave_addr in self.i2c.scan(), "Did not find slave %d in scan" % slave_addr
         self.addr = slave_addr
         # received data from temperature and humidity registers is two unsigned characters
-        self.fmt = '>2B'
+        #self.fmt = '>2B'
         # Sleep for 15 ms to allow the temperature and humidity temperatures to start recording
         # Only serial number registers 0xFB and 0xFF are available at first
-        sleep(0.015)
+        sleep_ms(STARTUP_DELAY)
         # set up for 14 bit resolution (in config register) for both temperature and humidity readings
         # independent measurements for now
         data = bytearray(3)
@@ -60,35 +62,28 @@ class HDC1080:
         i2c.writeto(self.addr, data)
 
     def read_temperature(self):
-        """ Read the temperature
-        Keyword arguments:
-        celsius -- If the data is kept as celsius after reading (default False)
-        """
         # write to the pointer register, changing it to the temperature register
         self.i2c.writeto(self.addr, bytearray([TEMP_REG]))
-        # TODO: Waiting for a temperature update here may not be necessary, based on 14-bit resolution
-        #sleep(0.0635)
+        sleep_ms(MEAS_DELAY)
         # per the spec, the conversion to celsius is (value / (2 ** 16)) * 165 - 40
         return ((int.from_bytes(self.i2c.readfrom(self.addr, 2), "big")) * 165 / 65536 - 40)
 
     def read_humidity(self):
-        """ Read the relative humidity """
         # write to the pointer register, changing it to the humidity register
         self.i2c.writeto(self.addr, bytearray([HUMI_REG]))
-        # TODO: Waiting for a humidity update here may not be necessary, based on 14-bit resolution
-        #sleep(0.065)
+        sleep_ms(MEAS_DELAY)
         return ((int.from_bytes(self.i2c.readfrom(self.addr, 2), "big")) / 65536 * 100) # conversion per the spec
 
     def read_configuration_register(self):
         self.i2c.writeto(self.addr, bytearray([CONF_REG]))
-        #sleep(0.0625)
+        sleep_ms(MEAS_DELAY)
         return (int.from_bytes(self.i2c.readfrom(self.addr, 2), "big"))
 
     def turnHeaterOn(self):
         config_data = bytearray(3)
         config_data[0] = CONF_REG
         config_data[1] = self.read_configuration_register()
-        config_data[1] = config_data | HDC1080_CONFIG_HEATER_ENABLE 
+        config_data[1] = config_data | HEATER_ENABLE 
         i2c.writeto(self.addr, config_data)
         return
 
@@ -96,7 +91,7 @@ class HDC1080:
         config_data = bytearray(3)
         config_data[0] = CONF_REG
         config_data[1] = self.read_configuration_register()
-        config_data[1] = (config_data & ~HDC1080_CONFIG_HEATER_ENABLE) >> 8
+        config_data[1] = (config_data & ~HEATER_ENABLE) >> 8
         i2c.writeto(self.addr, config_data)
         return
 
@@ -104,7 +99,7 @@ class HDC1080:
         config_data = bytearray(3)
         config_data[0] = CONF_REG
         config_data[1] = self.read_configuration_register()
-        config_data[1] = ((config_data & ~0x300) | resolution) >> 8 
+        config_data[1] = ((config_data & ~(UM_RES_11BIT | HUM_RES_14BIT | HUM_RES_8BIT)) | resolution) >> 8 
         i2c.writeto(self.addr, config_data)
         return
 
@@ -112,28 +107,28 @@ class HDC1080:
         config_data = bytearray(3)
         config_data[0] = CONF_REG
         config_data[1] = self.read_configuration_register()
-        config_data[1] = ((config_data & ~0x0400) | resolution) >> 8 
+        config_data[1] = ((config_data & ~(TEMP_RES_11BIT | TEMP_RES_14BIT)) | resolution) >> 8 
         i2c.writeto(self.addr, config_data)
         return
 
     def readManufacturerID(self):
         self.i2c.writeto(self.addr, bytearray([MFID_REG]))
-        #sleep(0.0625)              # From the data sheet
+        sleep_ms(MEAS_DELAY)              # From the data sheet
         return (int.from_bytes(self.i2c.readfrom(self.addr, 2), "big"))
 
     def readDeviceID(self):
         self.i2c.writeto(self.addr, bytearray([DVID_REG]))
-        #sleep(0.0625)              # From the data sheet
+        sleep_ms(MEAS_DELAY)              # From the data sheet
         return (int.from_bytes(self.i2c.readfrom(self.addr, 2), "big"))
 
     def readSerialNumber(self):
         self.i2c.writeto(self.addr, bytearray([FSER_REG]))
-        #sleep(0.0625)              # From the data sheet
+        sleep_ms(MEAS_DELAY)             # From the data sheet
         serial_number = (int.from_bytes(self.i2c.readfrom(self.addr, 2), "big"))
         self.i2c.writeto(self.addr, bytearray([MSER_REG]))
-        #sleep(0.0625)              # From the data sheet
+        sleep_ms(MEAS_DELAY)               # From the data sheet
         serial_number = serial_number * 256 + (int.from_bytes(self.i2c.readfrom(self.addr, 2), "big"))
         self.i2c.writeto(self.addr, bytearray([LSER_REG]))
-        #sleep(0.0625)              # From the data sheet
+        sleep_ms(MEAS_DELAY)              # From the data sheet
         serial_number = serial_number * 256 + (int.from_bytes(self.i2c.readfrom(self.addr, 2), "big"))
         return serial_number
--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from machine import Pin, I2C
 from hdc1080 import HDC1080
 
 i2c = I2C(scl=Pin(22), sda=Pin(21))
-hdc1080 = HDC1080(i2c)
+sensor = HDC1080(i2c)
 
+def temp():
+    print(sensor.read_temperature())
+
+def hum():
+    print(sensor.read_humidity())
